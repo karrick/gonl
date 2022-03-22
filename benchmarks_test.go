1e9cc5616cb79efc81f2b326471df96dedbf6914
Karrick S. McDermott <karrick@karrick.dev>
2022-03-22 19:08:12 -0400

    refactored benchmark code

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -7,17 +7,24 @@ import (
 	"testing"
 )
 
+// bufSize will be used when making buffers for copying byte slices,
+// and its size is chosen based on what io.Copy will create when given
+// an empty slice.
+const bufSize = 32 * 1024
+
 // copyBuffer is a modified version of similarly named function in
-// standard library, provided here to prevent it from using
-// ReaderFrom.
+// standard library, provided here to be able to test using
+// io.CopyBuffer both with and without sending it a destination
+// structure that implements io.ReaderFrom.
 func copyBuffer(dst io.Writer, src io.Reader, buf []byte) (int64, error) {
 	var written int64
 	var err error
 
 	if buf == nil {
-		buf = make([]byte, 32*1024) // use same size as copy
+		buf = make([]byte, bufSize)
 	}
 
+	// See copyBuffer in io standard library.
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
@@ -50,13 +57,15 @@ func copyBuffer(dst io.Writer, src io.Reader, buf []byte) (int64, error) {
 
 func BenchmarkCheapWrites(b *testing.B) {
 	b.Run("BatchLineLineWriter", func(b *testing.B) {
-		const threshold = 32 * 1024 // use same size as copy
+		// These functions contrasts the benefit of BatchLineWriter
+		// having ReadFrom method available rather than only having
+		// Write method.
 
 		b.Run("ReadFrom", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				drain := new(discardWriteCloser)
 
-				output, err := NewBatchLineWriter(drain, threshold)
+				output, err := NewBatchLineWriter(drain, bufSize)
 				if err != nil {
 					b.Fatal(err)
 				}
@@ -76,15 +85,17 @@ func BenchmarkCheapWrites(b *testing.B) {
 			}
 		})
 		b.Run("Write", func(b *testing.B) {
+			buf := make([]byte, bufSize)
+
 			for i := 0; i < b.N; i++ {
 				drain := new(discardWriteCloser)
 
-				output, err := NewBatchLineWriter(drain, threshold)
+				output, err := NewBatchLineWriter(drain, bufSize)
 				if err != nil {
 					b.Fatal(err)
 				}
 
-				_, err = copyBuffer(output, bytes.NewReader(novel), nil)
+				_, err = copyBuffer(output, bytes.NewReader(novel), buf)
 				if err != nil {
 					b.Fatal(err)
 				}
@@ -102,11 +113,13 @@ func BenchmarkCheapWrites(b *testing.B) {
 
 	b.Run("PerLineWriter", func(b *testing.B) {
 		b.Run("Write", func(b *testing.B) {
+			buf := make([]byte, bufSize)
+
 			for i := 0; i < b.N; i++ {
 				drain := new(discardWriteCloser)
 				output := &PerLineWriter{WC: drain}
 
-				_, err := copyBuffer(output, bytes.NewReader(novel), nil)
+				_, err := copyBuffer(output, bytes.NewReader(novel), buf)
 				if err != nil {
 					b.Fatal(err)
 				}
@@ -132,14 +145,12 @@ func BenchmarkWorkingWrites(b *testing.B) {
 	var mac = []byte("\xfav\x96\xd1C\xea\xb4\xdd߿\xd0G\x0e\x95\xa8)\xb5\xed\xe6\x11{e\xf2f\xd2\xea\xf5\xdb=\xb46\xff")
 
 	b.Run("BatchLineLineWriter", func(b *testing.B) {
-		const threshold = 32 * 1024 // use same size as copy
-
 		b.Run("ReadFrom", func(b *testing.B) {
 			drain := newHashWriteCloser(key)
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				output, err := NewBatchLineWriter(drain, threshold)
+				output, err := NewBatchLineWriter(drain, bufSize)
 				if err != nil {
 					b.Fatal(err)
 				}
@@ -161,16 +172,17 @@ func BenchmarkWorkingWrites(b *testing.B) {
 			}
 		})
 		b.Run("Write", func(b *testing.B) {
+			buf := make([]byte, bufSize)
 			drain := newHashWriteCloser(key)
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
-				output, err := NewBatchLineWriter(drain, threshold)
+				output, err := NewBatchLineWriter(drain, bufSize)
 				if err != nil {
 					b.Fatal(err)
 				}
 
-				_, err = copyBuffer(output, bytes.NewReader(novel), nil)
+				_, err = copyBuffer(output, bytes.NewReader(novel), buf)
 				if err != nil {
 					b.Fatal(err)
 				}
@@ -190,13 +202,14 @@ func BenchmarkWorkingWrites(b *testing.B) {
 
 	b.Run("PerLineWriter", func(b *testing.B) {
 		b.Run("Write", func(b *testing.B) {
+			buf := make([]byte, bufSize)
 			drain := newHashWriteCloser(key)
 			b.ResetTimer()
 
 			for i := 0; i < b.N; i++ {
 				output := &PerLineWriter{WC: drain}
 
-				_, err := copyBuffer(output, bytes.NewReader(novel), nil)
+				_, err := copyBuffer(output, bytes.NewReader(novel), buf)
 				if err != nil {
 					b.Fatal(err)
 				}
